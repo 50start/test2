2f4b3f0f866f8c7f5a723b0caec2bed8128a3e84
EC2 Default User <ec2-user@ip-172-31-3-81.ap-northeast-1.compute.internal>
2021-02-18 12:04:50 +0000

    admin commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+ mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
  devise_for :users
   resources :users 
   resources :tests  
- mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
  root "tests#index"
  end
  
