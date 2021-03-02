b498bc5a1e28ce42f6cc8be5b5514dfc19051e86
EC2 Default User <ec2-user@ip-172-31-3-81.ap-northeast-1.compute.internal>
2021-03-02 13:18:50 +0000

    profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
  root "tests#index"
  devise_for :users
-  resources :users 
+  resources :users, only:[:show]
   resources :tests do
    resource :favorites, only:[:create, :destroy]
   end
